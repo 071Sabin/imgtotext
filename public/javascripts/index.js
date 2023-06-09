8e72938967506bea10bc4eb205409d2f4015b743
071Sabin <071.sabin.panthi@gmail.com>
2023-06-09 15:43:27 +0530

    2nd

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,1 @@
 const btn1 = document.getElementById('submitBtn');
-
-// btn1.addEventListener('click', function(){
-//     fetch('/hello', {method: 'POST'});
-//   console.log('hello threeee');
-// });
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
-// multer for file handling
 const multer = require('multer');
+const path = require('path');
 
-const upload = multer({dest: './public/uploads/'});
-const app = express();
+// Define the storage configuration for multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/'); // Specify the destination folder where images should be stored
+  },
+  
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname);
+    cb(null, Date.now() + extension); // Use the current timestamp as the filename to ensure uniqueness
+  },
+});
 
-// app.use(express.static('public'));
+
+const fileFilter = (req, file, cb) => {
+  // Check if the file type is an image
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true); // Accept the file
+  } else {
+    cb(new Error("manual: you are not uploading image")); // Reject the file
+  }
+};
+
+// Configure multer with the storage settings
+const upload = multer({ storage: storage, fileFilter: fileFilter});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image To Text' });
+  res.render('index', { title: 'Image To Text', fileFilter: fileFilter});
 });
 
-router.post('/converted',upload.array("files"), function(req, res, next){
-  res.send("file uploaded");
+router.post('/converted', upload.array('files'), function(req, res, next) {
+  res.redirect('/');
 });
+
 
 
 module.exports = router;
