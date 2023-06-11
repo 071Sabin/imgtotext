e2810046ded705bce932ce6b26b971c7a01779af
071Sabin <071.sabin.panthi@gmail.com>
2023-06-11 23:06:31 +0530

    user with unique cookie id

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,37 @@
 var express = require('express');
-var router = express.Router();
+// var router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const Tesseract = require('node-tesseract-ocr');
+const bcrypt = require('bcrypt'); //encyypt and decrypt
 // const { v4: uuidv4 } = require('uuid'); //used to create random texts of 32 places.
 
 const app=express();
 app.use(cookieParser());
 
-
-const generateUserId = function(req, res, next){
+function generateUserId(res){
   const userRandomId = Math.random().toString(36).substr(2, 9);
-  res.cookie('userId', generateUserId(), { maxAge: 30 * 24 * 60 * 60 * 1000 }, { httpOnly: true, sameSite: 'strict' });
   return`user_${userRandomId}`;
 }
 
-
 // Middleware to check if user ID cookie is set, and if not, generate a new one
-// app.use((req, res, next) => {
-//   if (!req.cookies.userId) {
-//     const userId = generateUserId();
-//     console.log("cookie userid: " + userId);
-//     res.cookie('userId', userId, { maxAge: 30 * 24 * 60 * 60 * 1000 }, { httpOnly: true, sameSite: 'strict' }); // Expires in 30 days:: { httpOnly: true, sameSite: 'strict' }
-//   }else{
-//     console.log("user id in cookie is already SET!!!!!!!!");
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  if (req.cookies.userName === undefined) {
+    const userId = generateUserId();
+    res.cookie('userName', userId, { maxAge: 30 * 24 * 60 * 60 * 1000 }, { httpOnly: true, sameSite: 'strict' }); // Expires in 30 days:: { httpOnly: true, sameSite: 'strict' }
+  }
+  next();
+});
 
 // Define the storage configuration for multer
+// each file upload goes through this process.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const userId = req.cookies.userId;
-    // console.log("|||user id from cookie is===> ", userId);
-    
+    const userId = req.cookies.userName;
+    console.log("*******user name from cookie is===> ", userId);
+
     // creating a unique folder for every users.
     const folderName = `./public/uploads/${userId}`;
     const imgFolder = path.join(folderName, 'images');
@@ -74,20 +70,18 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage: storage, fileFilter: fileFilter});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Image To Text', errorMessage: null});
 });
 
 // write upload.array('input field name', maxcount)
-router.post('/converted', upload.array('files'), function(req, res, next) {
+app.post('/converted', upload.array('files'), function(req, res, next) {
 
   if (req.fileValidationError) {
     return res.render('index', { title: 'Image To Text', errorMessage: req.fileValidationError});
   }
 
   else{
-    // return res.render('index', { title: 'Image To Text', errorMessage: null});
-    // res.cookie('userId', generateUserId(), { maxAge: 30 * 24 * 60 * 60 * 1000 }, { httpOnly: true, sameSite: 'strict' });
     const inpvalue = req.body.language;
     let lng = "";
     if(inpvalue === '1'){
@@ -129,4 +123,4 @@ router.post('/converted', upload.array('files'), function(req, res, next) {
 
 
 
-module.exports = router;
+module.exports = app;
